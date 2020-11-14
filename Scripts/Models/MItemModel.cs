146e07fecec6a351972bd0d50755fef1bd1b81a1
hayamingongo <wakutima.9@gmail.com>
2020-11-14 22:03:35 +0900

    item取得ロジック追加

---
+++ Scripts/Models/MItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PakuPakuOnLine.Model
+{
+    public class MItemModel
+    {
+        public long id {get; set;} 
+        public string name {get; set;} 
+        public string item_group_cd { get; set;} 
+        public long price { get; set;} 
+        public long value { get; set;} 
+        public int rarity { get; set;} 
+        public int rarityeffect_type { get; set;} 
+    }
+}
---
+++ Scripts/Repository/MItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using PakuPakuOnLine.Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace PakuPakuOnLine.Repository
+{
+    class MItemRepository
+    {
+        public List<MItemModel> GetMItemAll(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var resultList = new List<MItemModel>();
+
+                connection.Open();
+                var queryString = "SELECT * FROM dbo.m_item";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                var result = command.ExecuteReader();
+                while (result.Read())
+                {
+                    MItemModel model = new MItemModel();
+
+                    model.id = result.GetInt64(0);
+                    model.name = result.GetString(1);
+                    model.item_group_cd = result.GetString(2);
+                    model.price = result.GetInt64(3);
+                    model.value = result.GetInt64(4);
+                    model.rarity = result.GetInt32(5);
+                    model.rarityeffect_type = result.GetInt32(6);
+
+                    resultList.Add(model);
+                }
+
+                return resultList;
+            }
+        }
+    }
+}
